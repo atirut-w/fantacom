5fb90a2c1b4040dfe27add158380157f4b883871
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-11-26 19:50:28 +0700

    Arguments stub

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <argparse/argparse.hpp>
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    argparse::ArgumentParser parser("fantacom");
+    parser.add_description("Fantasy Computer emulator");
+
+    // Boot ROM image
+    parser.add_argument("bootrom")
+        .help("Boot ROM image");
+
+    // How many banks of 4KiB RAM to allocate
+    parser.add_argument("-r", "--ram")
+        .help("Number of 4KiB RAM banks to allocate")
+        .default_value(4)
+        .scan<'i', int>();
+
+    try
+    {
+        parser.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error &err)
+    {
+        std::cout << err.what() << std::endl;
+        std::cout << parser;
+        exit(0);
+    }
+
     return 0;
 }
