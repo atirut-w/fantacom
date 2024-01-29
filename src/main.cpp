baeb118af4b4e3a2233cebba406257662be47032
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-01-29 15:19:31 +0700

    Refactor argument parsing into separate function

--- src/main.cpp
+++ src/main.cpp
@@ -4,53 +4,61 @@
 #include <fstream>
 #include <filesystem>
 #include <raylib.h>
+#include <memory>
 
-using namespace std;
-
-int main(int argc, char *argv[])
+std::shared_ptr<const argparse::ArgumentParser> parse_args(int argc, char *argv[])
 {
-    argparse::ArgumentParser parser("fantacom");
-    parser.add_description("Fantasy Computer emulator");
+    auto parser = std::make_shared<argparse::ArgumentParser>("fantacom");
+    parser->add_description("Fantasy Computer emulator");
 
     // Boot ROM image
-    parser.add_argument("bootrom")
+    parser->add_argument("bootrom")
         .help("Boot ROM image");
-
+    
     // How many banks of 4KiB RAM to allocate
-    parser.add_argument("-r", "--ram")
+    parser->add_argument("-r", "--ram")
         .help("Number of 4KiB RAM banks to allocate")
         .default_value(4)
         .scan<'i', int>();
     
     // Target CPU frequency in MHz
-    parser.add_argument("-f", "--frequency")
+    parser->add_argument("-f", "--frequency")
         .help("Target CPU frequency in MHz")
         .default_value(1)
         .scan<'i', int>();
-
+    
     try
     {
-        parser.parse_args(argc, argv);
+        parser->parse_args(argc, argv);
     }
     catch (const std::runtime_error &err)
     {
         std::cout << err.what() << std::endl;
-        std::cout << parser;
+        std::cout << *parser;
         exit(1);
     }
 
+    return parser;
+}
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    auto parser = parse_args(argc, argv);
+
     Machine machine;
-    machine.ram.resize(parser.get<int>("--ram") * 0x1000);
+    machine.ram.resize(parser->get<int>("--ram") * 0x1000);
 
     {
-        ifstream bootrom(parser.get<string>("bootrom"), ios::binary);
+        ifstream bootrom(parser->get<string>("bootrom"), ios::binary);
 
         if (!bootrom)
         {
             cerr << "Could not open boot ROM image" << endl;
             exit(1);
         }
-        if (filesystem::file_size(parser.get<string>("bootrom")) > machine.rom.size())
+        if (filesystem::file_size(parser->get<string>("bootrom")) > machine.rom.size())
         {
             cerr << "WARN: Boot ROM image too large, truncating" << endl;
         }
@@ -60,7 +68,7 @@ int main(int argc, char *argv[])
 
     InitWindow(640, 480, "Fantacom - Initialising...");
     SetTargetFPS(60);
-    int frequency = parser.get<int>("--frequency");
+    int frequency = parser->get<int>("--frequency");
     int runover = 0; // The CPU is instruction-stepped, so we need to keep track of how many cycles we've run over by
 
     while (!WindowShouldClose())
