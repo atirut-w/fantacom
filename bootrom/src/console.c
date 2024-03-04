bdd2696eda66c94aacab67309705028d847237ab
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-03-04 22:38:32 +0700

    Correct console behavior

--- bootrom/src/console.c
+++ bootrom/src/console.c
@@ -29,20 +29,6 @@ __asm
 __endasm;
 }
 
-void scroll_char() __naked
-{
-__asm
-    ld bc, (80 * 25 * 2) - 2
-    ld de, 0x3000
-    ld hl, 0x3002
-    ldir
-    dec hl
-    ld (hl), 0
-
-    ret
-__endasm;
-}
-
 int fputc_cons_native(char c)
 {
     switch (c)
@@ -52,18 +38,22 @@ int fputc_cons_native(char c)
         screen[cursor].attribute = 0x07;
         
         if (cursor + 1 >= 80 * 25)
-            scroll_char();
+        {
+            scroll_line();
+            cursor -= cursor % 80;
+        }
         else
+        {
             cursor++;
+        }
         break;
-    case '\r':
-        cursor -= cursor % 80;
-        break; // TODO: Consider fall-through to '\n'
     case '\n':
         if (cursor + 80 >= 80 * 25)
             scroll_line();
         else
             cursor += 80;
+    case '\r':
+        cursor -= cursor % 80;
         break;
     }
 }
