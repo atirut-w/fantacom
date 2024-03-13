85fb37a861c6513afa45806cf1d37d3c5ff71a65
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-03-13 17:04:12 +0700

    Slight disk driver cleanup

--- bootrom/src/disk.c
+++ bootrom/src/disk.c
@@ -2,6 +2,17 @@
 #include <z80io.h>
 #include <string.h>
 
+enum DiskCommand
+{
+    COMMAND_GET_PRESENCE,
+    COMMAND_GET_LAST_SECTOR,
+    COMMAND_GET_STATUS,
+    
+    COMMAND_SEEK,
+    COMMAND_READ,
+    COMMAND_WRITE
+};
+
 void disk_select(char disk)
 {
     outc_port(0x0300, disk);
@@ -20,12 +31,12 @@ void disk_set_data(int data)
 
 bool disk_check_presense()
 {
-    return disk_send_command(0) > 0;
+    return disk_send_command(COMMAND_GET_PRESENCE) > 0;
 }
 
 int disk_get_status()
 {
-    return disk_send_command(2);
+    return disk_send_command(COMMAND_GET_STATUS);
 }
 
 void disk_wait_idle()
@@ -35,17 +46,17 @@ void disk_wait_idle()
 
 int disk_get_last_sector()
 {
-    return disk_send_command(1);
+    return disk_send_command(COMMAND_GET_LAST_SECTOR);
 }
 
 void disk_seek(int sector)
 {
     disk_set_data(sector);
-    disk_send_command(3);
+    disk_send_command(COMMAND_SEEK);
 }
 
 void disk_read(void *data)
 {
     disk_set_data(data);
-    disk_send_command(4);
+    disk_send_command(COMMAND_READ);
 }
