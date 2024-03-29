8ff4610ad3b08cf303f20f5a937d30558660495e
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-03-29 13:59:27 +0700

    Fix incorrect auto increment logic

--- src/io_devices/ici.cpp
+++ src/io_devices/ici.cpp
@@ -11,12 +11,15 @@ uint8_t ICI::in(uint16_t addr)
     {
         if (addr == offsetof(Registers, data))
         {
-            registers.address += registers.auto_increment;
             if (devices[registers.device_select])
             {
-                return devices[registers.device_select]->read(registers.address);
+                registers.data = devices[registers.device_select]->read(registers.address);
             }
-            return 0xff;
+            else
+            {
+                registers.data = 0xff;
+            }
+            registers.address += registers.auto_increment;
         }
 
         return ((uint8_t *)&registers)[addr];
@@ -30,11 +33,11 @@ void ICI::out(uint16_t addr, uint8_t val)
     {
         if (addr == offsetof(Registers, data))
         {
-            registers.address += registers.auto_increment;
             if (devices[registers.device_select])
             {
                 devices[registers.device_select]->write(registers.address, val);
             }
+            registers.address += registers.auto_increment;
         }
 
         ((uint8_t *)&registers)[addr] = val;
