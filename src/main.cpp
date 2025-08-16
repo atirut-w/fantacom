a336ee61fbe0d1bb5a321879f84e9b0836103b28
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-08-16 14:28:16 +0700

    Add argument parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,26 @@
+#include "argparse/argparse.hpp"
+#include <memory>
+
+using argparse::ArgumentParser;
+
+std::unique_ptr<const ArgumentParser> parse_args(int argc, char *argv[]) {
+  auto parser = std::make_unique<ArgumentParser>("fantacom");
+
+  try {
+    parser->parse_args(argc, argv);
+    return parser;
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    std::cerr << *parser;
+    return nullptr;
+  }
+}
+
 int main(int argc, char *argv[]) {
-  //
+  auto args = parse_args(argc, argv);
+  if (!args) {
+    return 1;
+  }
+
   return 0;
 }
